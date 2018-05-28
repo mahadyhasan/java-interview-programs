c5b8c39bf62414765e8cd2dca83ab64d772628e3
mahadyhasan <mahady.hasan@hotmail.com>
2018-05-28 15:20:54 +0100

    second commit

--- src/main/java/com/exercise/strings/StringReverseRecursion.java
+++ src/main/java/com/exercise/strings/StringReverseRecursion.java
@@ -1,4 +1,22 @@
 package com.exercise.strings;
 
 public class StringReverseRecursion {
+
+    String reverse = "";
+
+    private String reverseString(String str) {
+        if (str.length() == 1) {
+            return str;
+        } else {
+            reverse += str.charAt(str.length() - 1)
+                    + reverseString(str.substring(0, str.length() - 1));
+            return reverse;
+        }
+    }
+
+    public static void main(String[] args) {
+        StringReverseRecursion stringReverseRecursion = new StringReverseRecursion();
+        System.out.println("Result: " + stringReverseRecursion.reverseString("Java2Novice"));
+    }
 }
+
